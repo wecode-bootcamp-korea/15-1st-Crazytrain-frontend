a9f8c6439be5e9d752e635fce5576fcda76ba212
김영재 <gim-yeongjae@gim-yeongjaeui-MacBookPro.local>
2020-12-16 12:44:40 +0900

    Add: 로그인,회원가입 레이아웃 추가

--- src/Pages/Account/Login/Login.js
+++ src/Pages/Account/Login/Login.js
@@ -1,12 +1,84 @@
 import React from "react";
+import "./login.scss";
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      pw: "",
+      token: "",
+    };
   }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    return <h1>haha</h1>;
+    return (
+      <div className="mainLogin">
+        <div className="loginContainer">
+          <div className="header">
+            <img
+              className="loginLogo addCursor"
+              src="/images/login/todayhome.png"
+              alt="todayhome"
+            />
+          </div>
+          <div className="loginBox">
+            <input
+              className="loginId"
+              type="text"
+              placeholder="이메일"
+              onChange={this.handleInputChange}
+            />
+            <input
+              className="loginPw"
+              type="password"
+              placeholder="비밀번호"
+              onchange={this.handleInputChange}
+            />
+            <button className="loinBtn addCursor">로그인</button>
+          </div>
+
+          <div className="loginInfo">
+            <span className="rePassword">비밀번호 재설정</span>
+            <span className="infoSignUp">회원가입</span>
+          </div>
+          <div className="loginSns">
+            <div className="explainIcon">SNS계정으로 간편 로그인/회원가입</div>
+            <div className="loginSnsList">
+              <img
+                className="loginIconList"
+                src="/images/login/facebook.png"
+                alt="facebook"
+                width="48"
+                height="48"
+              />
+              <img
+                className="loginIconList"
+                src="/images/login/kakao.png"
+                alt="kakao"
+                width="48"
+                height="48"
+              />
+              <img
+                className="loginIconList"
+                src="/images/login/naver.png"
+                alt="naver"
+                width="48"
+                height="48"
+              />
+            </div>
+          </div>
+          <div className="loginNomember">비회원 주문 조회하기</div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Pages/Account/Signup/Signup.js
+++ src/Pages/Account/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Signup.scss";
 
 class Signup extends React.Component {
   constructor(props) {
@@ -6,7 +7,117 @@ class Signup extends React.Component {
     this.state = {};
   }
   render() {
-    return <div>haha</div>;
+    return (
+      <div className="signUpFullPage">
+        <div className="smallContainer">
+          <img
+            className="signUpImg"
+            src="/images/login/todayhome.png"
+            alt="todayhome"
+          />
+          <div className="printContainer">
+            <div className="signUpContainer">
+              <div className="signUpHeader">
+                <div className="signUptitle">회원가입</div>
+                <div className="signUpExplain">
+                  SNS계정으로 간편하게 회원가입
+                </div>
+                <div className="signUpSnsImg">
+                  <img
+                    className="signUpIconImg"
+                    src="/images/login/facebook.png"
+                    alt="facebook"
+                  />
+                  <img
+                    className="signUpIconImg"
+                    src="/images/login/kakao.png"
+                    alt="kakao"
+                  />
+                  <img
+                    className="signUpIconImg"
+                    src="/images/login/naver.png"
+                    alt="naver"
+                  />
+                </div>
+              </div>
+              <div className="signUpEmail">
+                <div className="EmailTitle">이메일</div>
+                <span className="EmailInput">
+                  <input
+                    className="writeEmail"
+                    type="email"
+                    placeholder="이메일"
+                  />
+                  <span className="golbang">@</span>
+                  <input className="choiceEmail" type="text" />
+                </span>
+              </div>
+              <div className="signUpPassword">
+                <div className="passwordTitle">비밀번호</div>
+                <div className="explainPassword">8자 이상 입력해주세요</div>
+                <input
+                  className="writePassword"
+                  type="password"
+                  placeholder="비밀번호"
+                />
+              </div>
+              <div className="confirmPassword">
+                <div className="confirmTitle">비밀번호 확인</div>
+                <input
+                  className="reConfirmPassword"
+                  type="password"
+                  placeholder="비밀번호 확인"
+                />
+              </div>
+              <div className="nickName">
+                <div className="nickNameTitle">별명</div>
+                <div className="explainNickname">
+                  다른 유저와 겹치지 않는 별명을 입력해주세요. (2~15자)
+                </div>
+                <input
+                  className="writeNickname"
+                  type="text"
+                  placeholder="별명 (2~15자)"
+                />
+              </div>
+              <div className="agreement">약관 동의</div>
+              <div className="agreementBox">
+                <div className="okSign">
+                  <input className="checkBox" type="checkbox" />
+                  <span className="checkBoxSign">전체동의</span>
+                </div>
+                <div className="fourCheck">
+                  <div className="fourTeen">
+                    <input className="checkBox" type="checkbox" />
+                    <span className="moreFourTeen">만 14세 이상입니다.</span>
+                    <span className="essentional">(필수)</span>
+                  </div>
+                  <div className="explainRule">
+                    <input className="checkBox" type="checkbox" />
+                    <span className="explaingRuleUnderline">이용약관</span>
+                    <span className="essentional">(필수)</span>
+                  </div>
+                  <div className="personalInfo">
+                    <input className="checkBox" type="checkbox" />
+                    <span className="infoUser">개인정보처리방침</span>
+                    <span className="essentional">(필수)</span>
+                  </div>
+                  <div className="messageGet">
+                    <input className="checkBox" type="checkbox" />
+                    <span className="messageEvent">
+                      이벤트, 프로모션 알림 메일 및 SMS 수신.
+                    </span>
+                    <span className="essentional">(필수)</span>
+                  </div>
+                </div>
+                <button className="completeButton">회원가입 완료</button>
+                <div className="footer">이미 아이디가 있으신가요? 로그인</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
