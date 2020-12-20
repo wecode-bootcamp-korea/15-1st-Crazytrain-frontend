cc7e945b3cc3d400c45e22be192312056af45188
TonyK <sunghk0901@gmail.com>
2020-12-20 17:19:44 +0900

    Add: 레이아웃 작업중1

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Navigation.scss";
+
+class Navigation extends Component {
+  render() {
+    return <nav className="Navigation">여기는 네비게이션 영역입니다.</nav>;
+  }
+}
+
+export default Navigation;
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Community from "./Pages/Community/Community";
+import CommunityDetail from "./Pages/Community/CommunityDetail/CommunityDetail";
+import Navigation from "./Components/Navigation/Navigation";
 import Store from "./Pages/Store/Store";
 import Login from "./Pages/Account/Login/Login";
 import Signup from "./Pages/Account/Signup/Signup";
@@ -9,7 +11,9 @@ class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Navigation />
         <Switch>
+          <Route exact path="/" component={CommunityDetail} />
           <Route exact path="/community" component={Community} />
           <Route exact path="/store" component={Store} />
           <Route exact path="/login" component={Login} />
