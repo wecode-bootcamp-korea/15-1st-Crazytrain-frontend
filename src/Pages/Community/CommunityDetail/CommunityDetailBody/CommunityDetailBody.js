ea7f83241f344c20622b27cc4518ec018e755f28
TonyK <sunghk0901@gmail.com>
2020-12-21 12:08:19 +0900

    Add: 레이아웃 작업중3

--- src/Pages/Community/CommunityDetail/CommunityDetailBody/CommunityDetailBody.js
+++ src/Pages/Community/CommunityDetail/CommunityDetailBody/CommunityDetailBody.js
@@ -2,23 +2,48 @@ import React, { Component } from "react";
 import "./CommunityDetailBody.scss";
 
 class CommunityDetailBody extends Component {
+  state = {};
+
   render() {
     const { mainImage, markerList } = this.props.content;
     return (
       <section className="CommunityDetailBody">
-        <div className="bodyMainImage">
+        <div className="bodyMain">
           <img
-            className={"bodyMainImage"}
+            className="bodyMainImage"
             src={`${mainImage}`}
             alt={`${mainImage}`}
           />
           {markerList.map(marker => {
             return (
               <div
+                onMouseEnter={this.toggleMarker}
+                onMouseLeave={this.toggleMarker}
                 style={{ left: marker.x, top: marker.y }}
-                className="marker addCursor"
+                className={"marker addCursor"}
               >
                 +
+                <div className="bubbleWrapper">
+                  <div className="bubble">
+                    <div className="column">
+                      <img
+                        alt={marker.details.image}
+                        src={marker.details.image}
+                      />
+                      <section>
+                        <div>{marker.details.brandName}</div>
+                        <div>{marker.details.name}</div>
+                        <div>
+                          {`${marker.details.price
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                          원`}
+                        </div>
+                      </section>
+                    </div>
+                    <div className="column">여기는 화살표</div>
+                  </div>
+                </div>
               </div>
             );
           })}
